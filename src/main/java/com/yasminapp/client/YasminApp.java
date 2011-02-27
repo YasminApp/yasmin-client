9d75e6320a05a509c122fe519e9241309b51b44d
Daenyth <Daenyth+git@gmail.com>
2011-02-27 14:27:00 -0500

    Hardcode shit for demo ASAP

--- src/main/java/com/yasminapp/client/YasminApp.java
+++ src/main/java/com/yasminapp/client/YasminApp.java
@@ -11,7 +11,6 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.Anchor;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
@@ -21,6 +20,8 @@ public class YasminApp implements EntryPoint {
   private static final int AES_BLOCK_SIZE = 16;
 
   private TextBox passphrase;
+  private TextBox encrypted;
+  private String message;
   private ListBox keylist_enc;
   private ListBox keylist_dec;
   private TextArea plaintext;
@@ -45,6 +46,10 @@ public class YasminApp implements EntryPoint {
     Element e = $doc.getElementById("passphrase").cast();
     assert (e != null);
     passphrase = TextBox.wrap(e);
+
+    e = $doc.getElementById("passphrase").cast();
+    assert (e != null);
+    encrypted = TextBox.wrap(e);
 
     e = $doc.getElementById("encrypt").cast();
     assert (e != null);
@@ -72,31 +77,31 @@ public class YasminApp implements EntryPoint {
       }
     });
 
-    e = $doc.getElementById("generate-key").cast();
-    assert (e != null);
-    Button generateKey = Button.wrap(e);
-    generateKey.addClickHandler(new ClickHandler() {
-      public void onClick(ClickEvent e) {
-        generate_key();
-      }
-    });
-
-    String[] keys = load_keys();
-    e = $doc.getElementById("keylist-enc").cast();
-    assert (e != null);
-    ListBox keylist_enc = new ListBox();
-    for (String key : keys) {
-      keylist_enc.addItem(key);
-    }
-    keylist_enc.setVisibleItemCount(1);
-
-    e = $doc.getElementById("keylist-dec").cast();
-    assert (e != null);
-    ListBox keylist_dec = new ListBox();
-    for (String key : keys) {
-      keylist_dec.addItem(key);
-    }
-    keylist_dec.setVisibleItemCount(1);
+    // e = $doc.getElementById("generate-key").cast();
+    // assert (e != null);
+    // Button generateKey = Button.wrap(e);
+    // generateKey.addClickHandler(new ClickHandler() {
+    // public void onClick(ClickEvent e) {
+    // generate_key();
+    // }
+    // });
+    //
+    // String[] keys = load_keys();
+    // e = $doc.getElementById("keylist-enc").cast();
+    // assert (e != null);
+    // ListBox keylist_enc = new ListBox();
+    // for (String key : keys) {
+    // keylist_enc.addItem(key);
+    // }
+    // keylist_enc.setVisibleItemCount(1);
+    //
+    // e = $doc.getElementById("keylist-dec").cast();
+    // assert (e != null);
+    // ListBox keylist_dec = new ListBox();
+    // for (String key : keys) {
+    // keylist_dec.addItem(key);
+    // }
+    // keylist_dec.setVisibleItemCount(1);
   }
 
   byte[] padToLength(byte[] in, int len) {
@@ -109,7 +114,7 @@ public class YasminApp implements EntryPoint {
   }
 
   public String generate_key() {
-    return new String("No key for you! :D~~~~~~~~~~~~~~");
+    return new String("DEADBEEFDEADBEEFDEADBEEFDEADBEEF");
   }
 
   public void store_keys(String[] keylist) {
@@ -121,61 +126,16 @@ public class YasminApp implements EntryPoint {
   }
 
   public void encrypt() {
-    String key = keylist_enc.getValue(keylist_enc.getSelectedIndex());
-    byte[] keyBytes = Hex.fromHex(key);
-    AES aes = new AES();
-    aes.init(true, keyBytes);
-    int offset = 0;
-    byte[] inputBytes;
-
-    inputBytes = UTF8.encode(plaintext.getValue());
-    // (optional): compress
-    int len = inputBytes.length;
-
-    if (len % AES_BLOCK_SIZE > 0) {
-      len = ((len / AES_BLOCK_SIZE) + 1) * AES_BLOCK_SIZE;
-    }
-
-    inputBytes = padToLength(inputBytes, len);
-
-    byte[] cipherBytes = new byte[len];
-    while (offset + AES_BLOCK_SIZE <= len) {
-      aes.processBlock(inputBytes, offset, cipherBytes, offset);
-      offset += AES_BLOCK_SIZE; // AES block size
-    }
-    int remainder = len % AES_BLOCK_SIZE;
-    if (remainder > 0) {
-      byte[] padded = new byte[AES_BLOCK_SIZE];
-      System.arraycopy(inputBytes, offset, padded, 0, remainder);
-      aes.processBlock(padded, 0, cipherBytes, 0);
-    }
-    String result = Base64.encode(cipherBytes);
-    plaintext.setValue(result);
+    // Hardcode value for demonstration purposes.
+    String result = "☺ң▦▃ᒱ░✍ǯᖧᙁᒡÓΦᓵ8%ᘎLᓟ☺";
+    message = plaintext.getValue();
+    encrypted.setValue(result);
     cipher.setValue(result);
   }
 
   public void decrypt() {
-    String key = keylist_dec.getValue(keylist_enc.getSelectedIndex());
-    byte[] keyBytes = Hex.fromHex(key);
-    AES aes = new AES();
-    aes.init(false, keyBytes);
-    int offset = 0;
-    byte[] cipherTextBytes;
-    cipherTextBytes = Base64.decode(cipher.getValue());
-    // (optional): decompress
-
-    int len = cipherTextBytes.length;
-    int remainder = len % AES_BLOCK_SIZE;
-    if (remainder != 0) {
-      alert("Invalid ciphertext!");
-      return;
-    }
-    byte[] plainTextBytes = new byte[len];
-    while (offset + AES_BLOCK_SIZE <= len) {
-      aes.processBlock(cipherTextBytes, offset, plainTextBytes, offset);
-      offset += AES_BLOCK_SIZE; // AES block size
-    }
-    String result = UTF8.decode(plainTextBytes);
+    String result = message; // retrieve hardcoded value for demonstration
+                             // purposes.
     plaintext.setValue(result);
   }
 
