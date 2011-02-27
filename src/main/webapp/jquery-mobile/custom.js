edc5c0fac5c2f5469ee9eb46ee006340b92fd4e2
Yifei Zhang <yz@yifei.co>
2011-02-27 12:36:32 -0500

    polishing UI

--- src/main/webapp/jquery-mobile/custom.js
+++ src/main/webapp/jquery-mobile/custom.js
@@ -1,7 +1,7 @@
 $(document).bind('mobileinit', function(){
   
   $('#home').live('pagebeforecreate',function(event){
-    $('#splash').delay(1000).fadeOut(500);
+    $('#splash').delay(3000).fadeOut(500);
   });
   
 });
